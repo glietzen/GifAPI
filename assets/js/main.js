66a5b797344dfbab3f6fcd7b74bca98faa10b140
Garrett Lietzen <glietzen@gmail.com>
2018-02-18 16:39:20 -0600

    fixed issue with history btn
    added event delegation to make history-btn on click events work.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,7 @@ function addSearch() {
 }
 
 
-$('.history-btn').on('click', function() {
-    console.log('hello');
-});
+
 
 
 function returnSearch() {
@@ -30,6 +28,26 @@ $.ajax({
     console.log(queryURL);
     console.log(response);
     console.log(response.data.length);
+    for (var i =0; i < response.data.length; i++) {
+        arrImg = response.data[i].images.fixed_width.url;
+        var newContent = '';
+        newContent = '<img src="' + arrImg + '">';
+        $('.gif-area').html(newContent);
+        console.log(newContent);
+    }
+});
+}
+
+function returnHistory() {
+    var gifSearch = $(this).attr("data-name");
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=ZGm4k7GW4QuBRafZmsbPRaol8zqSLXZh&q=' + gifSearch + '&limit=15&offset=0&rating=PG&lang=en';
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(gifSearch);
+    // console.log(response);
+    // console.log(response.data.length);
     for (var i =0; i < response.data.length; i++) {
         arrImg = response.data[i].images.fixed_width.url;
         var newContent = '';
@@ -57,5 +75,7 @@ function renderGIFs () {
     $(".gif-area").html(newGifs);
     }
 }
+
+$('.prev-searches').on('click', '.history-btn', returnHistory);
 
 });
